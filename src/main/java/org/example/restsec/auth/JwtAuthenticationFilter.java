979ab693a05fd341320511a690b8dc1c089a89d3
jmin4078 <wpalsrl8588@gmail.com>
2026-08-18 16:04:08 +0900

    feat: add REST security error handling and CORS

---
+++ src/main/java/org/example/restsec/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package org.example.restsec.auth;
+
+public class JwtAuthenticationFilter {
+}
--- src/main/java/org/example/restsec/config/SecurityConfig.java
+++ src/main/java/org/example/restsec/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.restsec.config;
 
+import org.example.restsec.auth.RestAccessDeniedHandler;
+import org.example.restsec.auth.RestAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -21,11 +28,23 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults()) // CORS
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
+                .httpBasic(
+                        basic -> basic
+                                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+
+                )
+                .exceptionHandling(
+                        ex -> ex
+                                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                                .accessDeniedHandler(new RestAccessDeniedHandler())
+                )
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(
+                                        "/", "/index.html",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html",
                                         "/v3/api-docs/**")
@@ -35,7 +54,21 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers( HttpMethod.POST,"/chair/**")
                                 .authenticated()
+                                .requestMatchers( HttpMethod.DELETE,"/chair/**")
+                                .hasRole("ADMIN")
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        config.setAllowedMethods(List.of("GET", "POST", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
